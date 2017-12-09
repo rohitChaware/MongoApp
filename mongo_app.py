31ef7cb050f919c7ce327b6aa46076ae57090e63
Rohit Chaware <rohit.chaware@oracle.com>
2017-12-09 21:20:44 +0530

    Refactor app for book reviews collection
    - Add a form to get a new review.
    - Add style to the json-table.

--- mongo_app.py
+++ mongo_app.py
@@ -7,41 +7,53 @@ import os
 
 app = Flask(__name__)
 
-MONGO_DBNAME = 'test'
+MONGO_DBNAME = 'demo'
 app.config['MONGO_DBNAME'] = MONGO_DBNAME
 MONGO_URL = os.environ['MONGO_URL']
 app.config['MONGO_URI'] = 'mongodb://' + MONGO_URL + '/' + MONGO_DBNAME
 
 mongo = PyMongo(app)
 
+@app.route('/add')
+def new_entry():
+  return render_template('entry_form.html')
+
+@app.route('/test', methods=['GET'])
+def test():
+  output = {'/all': 'To get all records', '/add': 'To add a json record'}
+  return jsonify({'Success. Url Info' : output})
+
 @app.route('/all', methods=['GET'])
 def get_all():
-  zips = mongo.db.zips
+  reviews = mongo.db.reviews
   output = []
-  for s in zips.find():
-    output.append({'city' : s['city'], 'location' : s['loc'], 'state': s['state']})
+  for s in reviews.find():
+    output.append({'book' : s['book'], 'author' : s['author'], 'review': s['review']})
   return render_template('json_table.html',  posts=output)
 
-@app.route('/zip/<name>', methods=['GET'])
-def get_one_zip(name):
-  zips = mongo.db.zips
-  s = zips.find_one({'city' : name})
-  if s:
-    output = {'city' : s['city'], 'location' : s['loc'], 'state': s['state']}
+@app.route('/book/<name>', methods=['GET'])
+def get_book_review(name):
+  reviews = mongo.db.reviews
+  review_list = reviews.find({'book' : name})
+  if review_list:
+    output = []
+    for s in review_list:
+     output.append({'book' : s['book'], 'author' : s['author'], 'review': s['review']})
+    return render_template('json_table.html',  posts=output)
   else:
-    output = "No such city."
-  return jsonify({'result' : output})
+    output = "No such book."
+    return jsonify({'result' : output})
 
-@app.route('/zip', methods=['POST'])
-def add_zip():
-  zips = mongo.db.zips
-  city = request.json['city']
-  loc = request.json['loc']
-  state = request.json['state']
-  zip_id = zips.insert({'city': city, 'loc': loc, 'state':  state})
-  new_zip = zips.find_one({'_id': zip_id })
-  output = {'name' : new_zip['city'], 'location' : new_zip['loc'], 'state': new_zip['state']}
-  return jsonify({'result' : output})
+@app.route('/add', methods=['POST'])
+def add_book_review():
+  reviews = mongo.db.reviews
+  book = request.form['book']
+  author = request.form['author']
+  review = request.form['review']
+  review_id = reviews.insert({'book': book, 'author': author, 'review':  review})
+  new_review = reviews.find_one({'_id': review_id })
+  output = [{'book' : new_review['book'], 'author' : new_review['author'], 'review': new_review['review']}]
+  return render_template('json_table.html',  posts=output)
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(port=8080)
